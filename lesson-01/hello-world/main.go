44a9a84f0eb5e05b2d87174e89e82de1d2d56900
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-03-01 18:35:37 +0100

    Created frist program in Go

---
+++ lesson-01/hello-world/main.go
@@ -0,0 +1,17 @@
+package main
+
+//pachetto fmt (pronunci fumt)
+import "fmt"
+
+// Kyeword func
+// Funzione main entry point programma non accetta parametri
+func main() {
+	// Body funzione main
+	fmt.Println("Hello, World")
+}
+
+// Come runnare
+// go run <nome file>
+
+// go build
+// Builda il file, va fatto nella cartella dove c'è il file go.mod, praticamente fa fatto dentro al modulo
